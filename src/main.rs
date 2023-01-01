6de2103f734efc6a55fe54b855ee72513eb8d473
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2023-01-01 15:46:26 +0100

    Add argument parsing using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(version, about)]
+struct Args {
+    #[clap(subcommand)]
+    action: Action, 
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum Action {
+    /// Encrypt a file
+    Encrypt(Encrypt),
+
+    /// Decrypt a file
+    Decrypt(Decrypt),
+}
+
+#[derive(clap::Args, Debug)]
+struct Encrypt {
+    /// File to be encrypted
+    file: String,
+
+    /// Output file one. Defaults to "<FILE>.otp.0"
+    #[arg(short = '1', long)]
+    out1: Option<String>,
+    
+    /// Output file two. Defaults to "<FILE>.otp.1"
+    #[arg(short = '2', long)]
+    out2: Option<String>,
+
+    /// Buffer size in bytes
+    #[arg(long, default_value = "1048576")]
+    buffer: u32,
+}
+
+#[derive(clap::Args, Debug)]
+struct Decrypt {
+    /// Output file name. This is the name of the decrypted file.
+    file: String,
+
+    /// Input file one. Defaults to "<FILE>.otp.0"
+    #[arg(short = '1', long)]
+    in1: Option<String>, 
+    
+    /// Input file one. Defaults to "<FILE>.otp.1"
+    #[arg(short = '2', long)]
+    in2: Option<String>,
+
+    /// Buffer size in bytes
+    #[arg(long, default_value = "1048576")]
+    buffer: u32,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:?}", args);
 }
