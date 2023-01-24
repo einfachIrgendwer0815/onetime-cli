705761594fb55d0688885c29fe90b56a9994282a
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2023-01-24 18:28:08 +0100

    Add integration test

---
+++ tests/integration_tests.rs
@@ -0,0 +1,101 @@
+use std::ffi::OsString;
+use std::fs::{read_dir, remove_file, remove_dir, create_dir, copy};
+use std::fs::File;
+use std::io::Read;
+use std::str::FromStr;
+use assert_cmd::Command;
+use md5_rs::Context;
+
+
+const CARGO_BIN_NAME: &str = "onetime-cli";
+
+const FILES_DIR: &str = "./tests/files";
+const FILES_ORIG_DIR: &str = "./tests/files_original";
+
+const FILE1_PATH: &str = "./tests/files/file1.txt";
+
+
+fn copy_files(dir_name: &str) -> std::io::Result<()> {
+    for entry in read_dir(FILES_ORIG_DIR.to_owned() + "/" + dir_name)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        let mut new_file = OsString::from_str(FILES_DIR).unwrap();
+        new_file.push("/");
+        new_file.push(path.file_name().unwrap());
+       
+        if path.is_file() {
+            copy(path, new_file)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn clear_files() {
+    #[allow(unused_must_use)]
+    {
+        remove_dir(FILES_DIR);
+        create_dir(FILES_DIR);
+    }
+
+}
+
+fn get_md5_sum(path: &str) -> Result<[u8; 16], std::io::Error> {
+    let mut ctx = Context::new();
+
+    let mut file = File::open(path)?;
+    let mut buffer = [0u8; 4096];
+
+    loop {
+        let bytes = file.read(&mut buffer)?;
+
+        if bytes == 0 {
+            break;
+        }
+
+        ctx.read(&buffer[..bytes]);
+    }
+
+    Ok(ctx.finish())
+}
+
+#[test]
+fn test_encrpyt_decrypt() {
+    clear_files();
+    copy_files("test_encrypt_decrypt").unwrap();
+
+    let original_md5 = get_md5_sum(FILE1_PATH).unwrap();
+
+    // Encrypt command
+    let mut cmd = Command::cargo_bin(CARGO_BIN_NAME).unwrap();
+    let assert = cmd
+        .current_dir(FILES_DIR)
+        .arg("encrypt")
+        .arg("file1.txt")
+        .assert();
+
+    assert
+        .success()
+        .stdout("Successfully encrypted file1.txt\n")
+        .stderr("");
+
+    remove_file(FILE1_PATH).unwrap();
+
+    // Decrypt command
+    let mut cmd = Command::cargo_bin(CARGO_BIN_NAME).unwrap();
+    let assert = cmd
+        .current_dir(FILES_DIR)
+        .arg("decrypt")
+        .arg("file1.txt")
+        .assert();
+    
+    assert
+        .success()
+        .stdout("Successfully decrypted file1.txt\n")
+        .stderr("");
+
+    let md5_now = get_md5_sum(FILE1_PATH).unwrap();
+
+    assert_eq!(original_md5, md5_now);
+}
